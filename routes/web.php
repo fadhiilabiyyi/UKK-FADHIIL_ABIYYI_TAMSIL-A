730358a721afcbc9d2cd702f4d92964cc1800c8c
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-03-20 14:33:25 +0700

    Custom Guard Middleware

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 
 Route::controller(AuthenticationController::class)->group(function () {
     Route::middleware(['guest:officer,community'])->group(function () {
+        Route::get('/', 'landing')->name('landing-page');
         Route::get('/login', 'login')->name('login');
         Route::post('/login', 'authenticate')->name('authenticate');
         Route::get('/register', 'registerPage')->name('register-page');
@@ -29,4 +30,9 @@ Route::controller(AuthenticationController::class)->group(function () {
         Route::get('/home', 'home')->name('home');
         Route::post('/logout', 'logout')->name('logout');
     });
+});
+
+// Only Community can access this route
+Route::middleware(['auth:community,officer', 'checkGuard:community'])->group(function () {
+
 });
